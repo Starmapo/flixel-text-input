2a0943f6c0e1653b1e484d080f536b36a4e91917
Starmapo <fabian.xavier7@gmail.com>
2023-12-07 18:54:42 -0400

    Added "basic" sample project - Updated changelog - Updated .gitignore - Updated VS Code settings

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
+<!-- markdownlint-configure-file {"MD024": {"siblings_only": true}} -->
 # Changelog
 
 All notable changes to this project will be documented in this file.
@@ -10,10 +11,15 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - `flixel.addons.text.ui.FlxUINumericStepper`: A recreation of the "flixel-ui" numeric stepper that uses this library's text input
+- Added "basic" sample project
 
 ### Changed
 
 - Moved `FlxUITextInput` to the `flixel.addons.text.ui` package.
+
+### Fixed
+
+- Fixed a bug with touch input.
 
 ## 1.0.0 - 2023-12-04
 
---
+++ samples/basic/source/PlayState.hx
@@ -0,0 +1,68 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.addons.text.FlxTextInput;
+import flixel.system.FlxAssets;
+import flixel.util.FlxColor;
+
+class PlayState extends FlxState
+{
+	override public function create()
+	{
+		bgColor = 0xFF808080;
+
+		final textInput = new FlxTextInput(5, 5, 0, "This is a default text input.", 16);
+		add(textInput);
+
+		final textInput = new FlxTextInput(textInput.x, textInput.y + textInput.height + 5, FlxG.width - 20,
+			"This is a text input with fieldWidth and fieldHeight set.", 16);
+		textInput.fieldHeight = 44;
+		add(textInput);
+
+		final textInput = new FlxTextInput(textInput.x, textInput.y + textInput.height + 5, FlxG.width - 20, "This is a single-line text input.", 16);
+		textInput.multiline = false;
+		add(textInput);
+
+		final textInput = new FlxTextInput(textInput.x, textInput.y + textInput.height + 5, FlxG.width - 20, "This is a text input with a DYNAMIC type.", 16);
+		textInput.type = DYNAMIC;
+		add(textInput);
+
+		final textInput = new FlxTextInput(textInput.x, textInput.y + textInput.height + 5, FlxG.width - 20, "This is an unselectable text input.", 16);
+		textInput.selectable = false;
+		add(textInput);
+
+		final textInput = new FlxTextInput(textInput.x, textInput.y + textInput.height + 5, FlxG.width - 20,
+			"This is an unselectable text input with a DYNAMIC type.", 16);
+		textInput.type = DYNAMIC;
+		textInput.selectable = false;
+		add(textInput);
+
+		final textInput = new FlxTextInput(textInput.x, textInput.y + textInput.height + 5, FlxG.width - 20, "This is a formatted text input.");
+		textInput.setFormat(FlxAssets.FONT_DEBUGGER, 16, FlxColor.LIME);
+		textInput.italic = textInput.bold = true;
+		add(textInput);
+
+		final textInput = new FlxTextInput(textInput.x, textInput.y + textInput.height + 5, FlxG.width - 20, "This text input is aligned to the center.", 16);
+		textInput.alignment = CENTER;
+		add(textInput);
+
+		final textInput = new FlxTextInput(textInput.x, textInput.y + textInput.height + 5, FlxG.width - 20, "This text input is aligned to the right.", 16);
+		textInput.alignment = RIGHT;
+		add(textInput);
+
+		final textInput = new FlxTextInput(textInput.x, textInput.y + textInput.height + 5, FlxG.width - 20, "Maximum characters: 30", 16);
+		textInput.maxChars = 30;
+		add(textInput);
+
+		final textInput = new FlxTextInput(textInput.x, textInput.y + textInput.height + 5, FlxG.width - 20, "ONLY UPPERCASE LETTERS ALLOWED", 16);
+		textInput.restrict = "A-Z ";
+		add(textInput);
+
+		final textInput = new FlxTextInput(textInput.x, textInput.y + textInput.height + 5, FlxG.width - 20, "password", 16);
+		textInput.displayAsPassword = true;
+		add(textInput);
+
+		super.create();
+	}
+}
