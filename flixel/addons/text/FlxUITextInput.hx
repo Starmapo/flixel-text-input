3588611abdcee34497114cf4d62072ffa94f1f48
Starmapo <fabian.xavier7@gmail.com>
2023-12-14 22:58:04 -0400

    Removed deprecated `FlxUITextInput`

--- flixel/addons/text/FlxUITextInput.hx
+++
@@ -1,7 +0,0 @@
-package flixel.addons.text;
-
-/**
- * DEPRECATED - `FlxUITextInput` was moved to the `flixel.addons.text.ui` package.
- */
-@:deprecated("FlxUITextInput was moved to the package: `flixel.addons.text.ui`")
-typedef FlxUITextInput = flixel.addons.text.ui.FlxUITextInput;
